00420527bddd2eb996b72aacb04c5b160e7dca8f
Laprevote Emma <emma.laprevote@laplateforme.io>
2020-11-30 20:40:01 +0100

    Page index

--- index.php
+++ index.php
@@ -4,13 +4,172 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" tpe="text/css" href="css/discussion.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>THE FACTORY</title>
 </head>
+
 <body>
+<header>
+<nav>
+    <div class="nav-wrapper #000000 black">
+      <img id="factorylogo" src="images/factorylogo.png" alt="Logo The factory">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="index.php"><i class="large material-icons">grade</i></a></li>
+        <li><a href="inscription.php"><i class="large material-icons">person_add</i></a></li>
+        <li><a href="connexion.php"><i class="large material-icons">assignment_ind</i></a></li>
+        <li><a href="profil.php"><i class="large material-icons">face</i></a></li>
+        <li><a href="discussion.php"><i class="large material-icons">message</i></a></li>
+      </ul>
+    </div>
+  </nav>
+  </header>
 
+  <main id="presentFactory">
+    
+    <aside>
+      <section id="liens">
+        <a href="https://fr-fr.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com/?hl=fr" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://twitter.com/?lang=fr" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.deviantart.com/" target="_blank"><i class="fab fa-deviantart"></i></a>
+      </section>
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <section id="biography">
+          <p id="textbio" class="flow-text"><span class="typo">the factory</span><br> est une communauté artistique spécialisée dans 
+          le graffiti en ligne, où chacun peut s'inscrire et exposer ses propres créations, 
+          graphiques, photographiques, échanger sur les spots, le matériel, la musique et ainsi cottoyer les graffeurs tout autour du globe !</p>
+      </section>
+      <section id="music">
+        <h5>#musique.</h5>
+        <div>
+            <p><span class="weight">The Streets</span><br>None of Us Are Getting Out of This Life Alive.</p>
+            <img src="images/thestreets.jpg" id="streets" alt="pochette dernier album the streets">
+            <p class="p3">Neuf ans après son dernier album, THE STREETS revient avec Tame Impala sur un enthousiasmant single 
+            annonciateur d'une mixtape estivale. L'occasion de réentendre la gouaille de Mike Skinner. </p>
+        </div>
+        <div>
+            <p><span class="weight">Slowthai</span><br>Nothing great about Britain.</p>
+            <img src="images/slowthai.jpeg" id="slow" alt="pochette dernier album Slowthai">
+            <p class="p3">Tout ce que l'Angleterre a fait de plus énervé ces deux dernières années semble s'être passé le mot. 
+            Après le retour en grâce ce matin des kids de Shame, voilà que ce sale gosse de slowthai s'invite à la fête et 
+            annonce un nouvel album, attendu le 5 février prochain.</p>
+        </div>
+      </section>
+    </aside>
+
+    <section id="present">
+        <div id="graffiti">
+            <div class="slogan">
+            <p>LESS ODOR -</p>
+            <p class="p2">MORE</p>
+            <p class="p2">ACTION.</p>
+            </div>
+        </div>
+
+    <article>
+        <section id="shop">
+            <h5>#chooseyourweaponwisely.</h5>
+            <img src="images/logoflame.png" id="logoflame" alt="Logo Flame"><br>
+            <img src="images/spray.png" id="spraypaint" alt="Bombe peinture flame">
+            <p id="textFlame">Découvrez la <span class="weight">bombe de peinture Flame Orange</span>, à travers <span class="weight">120 couleurs</span> funky ! 
+                Il s'agit de la bombe la plus aboutie au monde dans sa catégorie : Sa valve ultra contrôlable, 
+                dévelopée spécialement pour le graffiti est équipée <span class="weight">d'un fat cap d'origine</span>, et permet d'obtenir 
+                des lignes de <span class="weight">2 à 30 centimètres</span>, par simple gestion de la pression. La bombe de peinture 
+                Flame Orange, vous propose une expérience de peinture rapide, associant le plaisir d'une 
+                application couvrante, gérable et résistante au meilleur prix : un pur plaisir !</p>
+            <a class="waves-effect #000000 black btn"><i class="material-icons left">flash_on</i>Decouvrir</a>
+        </section>
+
+        <section id="art">
+            <img src="images/persu.jpg" id="boatgraff" alt="Bateau avec graffiti">
+            <h6>Entretien</h6>
+            <h2>Persu à la recherche des spots perdus.</h2>
+            <p id="textFlame">Partir en roadtrip pour dénicher des spots vierges, faire des graffs, 
+                prendre des photos… et croiser des chats. C’est le quotidien de Persu, 
+                un graffeur en quête d’aventures qui partage sa passion des lieux abandonnés 
+                avec sa compagne, photographe.</p>
+            <p class="p4">Rob Zacny le 16.11.20</p>
+            <a class="waves-effect #1b5e20 #000000 black btn"><i class="material-icons left">flash_on</i>Lire l'article</a>
+        </section>
+    </article>
+
+    <section id="gallery">
+        <div class="artist">
+            <img src="images/artistcorner.png" id="corner" alt="logo artist corner">
+        </div>
+
+    <section class="expo">
+        <div class="prod1">
+            <img src="images/prod1.jpg" id="prod1" alt="tableau graffiti">
+            <p class="weight">ROYS32</p>
+            <p>Indémodable</p>
+            <p class="weight">30,00 €</p>
+            <a class="waves-effect #000000 black btn"><i class="material-icons left">flash_on</i>DETAILS</a>
+        </div>
+
+        <div class="prod2">
+            <img src="images/prod2.jpg" id="prod2" alt="tableau graffiti">
+            <p class="weight">EKIEM</p>
+            <p>No Mosquito</p>
+            <p class="weight">50,00 €</p>
+            <a class="waves-effect #000000 black btn"><i class="material-icons left">flash_on</i>DETAILS</a>
+        </div>
+
+        <div class="prod3">
+            <img src="images/prod3.jpg" id="prod3" alt="tableau graffiti">
+            <p class="weight">ANTISTATIK</p>
+            <p>The pilot</p>
+            <p class="weight">100,00 €</p>
+            <a class="waves-effect #000000 black btn"><i class="material-icons left">flash_on</i>DETAILS</a>
+        </div>
+
+        <div class="prod4">
+            <img src="images/prod4.jpg" id="prod4" alt="tableau graffiti">
+            <p class="weight">ANTISTATIK</p>
+            <p>Burning Face</p>
+            <p class="weight">100,00 €</p>
+            <a class="waves-effect #000000 black btn"><i class="material-icons left">flash_on</i>DETAILS</a>
+        </div>
+      </section>
+    </section>
+    </section>
+  </main>
+  
+  <footer class="page-footer #bdbdbd grey lighten-1">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <img src="images/logo2.PNG" id="logo2" alt=" logo entreprise the factory">
+                <section id="liens2">
+                    <a href="https://fr-fr.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/?hl=fr" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="https://twitter.com/?lang=fr" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.deviantart.com/" target="_blank"><i class="fab fa-deviantart"></i></a>
+                </section>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <ul>
+                  <li><a class="black-text darken-4" href="index.php"><i class="material-icons left">flash_on</i>ACCUEIL</a></li>
+                  <li><a class="black-text darken-4" href="inscription.php"><i class="material-icons left">flash_on</i>INSCRIPTION</a></li>
+                  <li><a class="black-text darken-4" href="connexion.php"><i class="material-icons left">flash_on</i>CONNEXION</a></li>
+                  <li><a class="black-text darken-4" href="profil.php"><i class="material-icons left">flash_on</i>MON COMPTE</a></li>
+                  <li><a class="black-text darken-4" href="discussion.php"><i class="material-icons left">flash_on</i>FORUM</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container #000000 black-text">
+            © 2020 Emma Laprevote
+            </div>
+          </div>
+        </footer>
+            
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>    
 <script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
