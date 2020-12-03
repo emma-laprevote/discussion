59a37ce3638899281ae76b2c0e52d812d265c34a
Laprevote Emma <emma.laprevote@laplateforme.io>
2020-12-03 11:24:47 +0100

    decoupage terminé

--- pages/connexion.php
+++ pages/connexion.php
@@ -7,36 +7,18 @@ $msg_error = "";
     {
         $msg_error = connectLogin();
     }
+    $root_images = "../images/";
+    $root_index = "../index";
+    $root_inscription = "inscription";
+    $root_connexion = "";
+    $root_discussion = "";
+    $root_css = '../css/';
+    $root_js = '../js/';
   ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" tpe="text/css" href="../css/discussion.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
-    <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">  
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONNEXION</title>
-</head>
+<?php $title = 'CONNEXION'; ?>
+<?php ob_start(); ?>
 
-<body>
-<header>
-<nav>
-    <div class="nav-wrapper #000000 black">
-      <img id="factorylogo" src="../images/factorylogo.png" alt="Logo The factory">
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="../index.php"><i class="large material-icons">grade</i></a></li>
-        <li><a href="inscription.php"><i class="large material-icons">person_add</i></a></li>
-        <li><a href="connexion.php"><i class="large material-icons">assignment_ind</i></a></li>
-        <li><a href="profil.php"><i class="large material-icons">face</i></a></li>
-        <li><a href="discussion.php"><i class="large material-icons">message</i></a></li>
-      </ul>
-    </div>
-  </nav>
-  </header>
+<?php require_once('../php/header.php');?>
 
   <main id="logJoin">
     
@@ -70,38 +52,10 @@ $msg_error = "";
     </div>
         
   </main>
-  
-  <footer class="page-footer #bdbdbd grey lighten-1">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <img src="../images/logo2.PNG" id="logo2" alt=" logo entreprise the factory">
-                <section id="liens2">
-                    <a href="https://fr-fr.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                    <a href="https://www.instagram.com/?hl=fr" target="_blank"><i class="fab fa-twitter"></i></a>
-                    <a href="https://twitter.com/?lang=fr" target="_blank"><i class="fab fa-instagram"></i></a>
-                    <a href="https://www.deviantart.com/" target="_blank"><i class="fab fa-deviantart"></i></a>
-                </section>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <ul>
-                  <li><a class="black-text darken-4" href="index.php"><i class="material-icons left">flash_on</i>ACCUEIL</a></li>
-                  <li><a class="black-text darken-4" href="inscription.php"><i class="material-icons left">flash_on</i>INSCRIPTION</a></li>
-                  <li><a class="black-text darken-4" href="connexion.php"><i class="material-icons left">flash_on</i>CONNEXION</a></li>
-                  <li><a class="black-text darken-4" href="profil.php"><i class="material-icons left">flash_on</i>MON COMPTE</a></li>
-                  <li><a class="black-text darken-4" href="discussion.php"><i class="material-icons left">flash_on</i>FORUM</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container #000000 black-text">
-            © 2020 Emma Laprevote
-            </div>
-          </div>
-        </footer>
+  <?php require_once('../php/footer.php');?>
             
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>    
-<script type="text/javascript" src="../js/materialize.min.js"></script>
-</body>
-</html>
+<script type="text/javascript" src="<?= $root_js ?>materialize.min.js"></script>
+<?php $content = ob_get_clean(); ?>
+
+<?php require_once('../php/template.php'); ?>
